247c67ed5c84d430e5b978ea9bcaa4c7d025053a
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-27 23:02:00 +0800

    Update main.py

--- mix_design/main.py
+++ mix_design/main.py
@@ -3,7 +3,7 @@ import streamlit as st
 page_bg_img = '''
 <style>
 body {
-background-image: url("mix_design/2960070_2_1.jpg");
+background-image: url("https://github.com/NTUT530/Streamlit_learning/blob/main/mix_design/2960070_2_1.jpg");
 background-size: cover;
 }
 </style>
