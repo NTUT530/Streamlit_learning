491565f283481f0a60dbe03ddc895022ba7ae9d5
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-28 00:00:12 +0800

    Update main.py

--- mix_design/main.py
+++ mix_design/main.py
@@ -15,7 +15,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.header('IS 10262 混凝土混合設計 - 混凝土混合比例')
+#st.header('IS 10262 混凝土混合設計 - 混凝土混合比例')
+st.write('<center>IS 10262 混凝土混合設計 - 混凝土混合比例</center>',unsafe_allow_html=True)
+
 col1,col2 = st.beta_columns(2)
 with col1:
     Grade_designation = st.selectbox('等級',['M10','M15','M20','M25','M30','M35','M40'])
