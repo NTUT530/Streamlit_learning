5a92bb4f5c9d11f9135c8b92e06c70248b42ac3e
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-28 00:18:32 +0800

    Update main.py

--- mix_design/main.py
+++ mix_design/main.py
@@ -16,7 +16,7 @@ st.markdown(
 )
 
 #st.header('IS 10262 混凝土混合設計 - 混凝土混合比例')
-st.write('<head style="color: Blue><center>IS 10262 混凝土混合設計 - 混凝土混合比例</center></head>',unsafe_allow_html=True)
+st.write('<span style="color: Blue><font size="24"><center>IS 10262 混凝土混合設計 - 混凝土混合比例</center></font></span>',unsafe_allow_html=True)
 
 col1,col2 = st.beta_columns(2)
 with col1:
