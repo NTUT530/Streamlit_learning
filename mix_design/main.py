ccd9c087f167d9657ff90071e28253817ea8be01
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-28 01:16:16 +0800

    Update main.py

--- mix_design/main.py
+++ mix_design/main.py
@@ -324,7 +324,7 @@ with col16:
     st.text_input('粗骨料',value=Str_CA)
 
 #st.latex(r'''質量體積比: (kg/m^3)''')
-st.write('<span style="color:Black;background:pink"><font size="6"><center><b>質量體積比: (kg/m^3)</center></font></span>',unsafe_allow_html=True)
+st.write('<span style="color:Black;background: White"><font size="6"><center><b>質量體積比: (kg/m^3)</center></font></span>',unsafe_allow_html=True)
 
 col17,col18,col19 = st.beta_columns(3)
 with col17:
