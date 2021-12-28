71b2bb1302ea5d5d498356fdcea27b3768376900
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-28 23:21:39 +0800

    Update main.py

--- mix_design/main.py
+++ mix_design/main.py
@@ -269,8 +269,8 @@ div.stButton > button:hover {
     color:#ff0000;
     }
 </style>""", unsafe_allow_html=True)
-co1, co2, co3 ,co4 ,co5 ,co6 ,co7= st.beta_columns(7)
-with co3:
+co1, co2, co3 ,co4 ,co5 ,co6 ,co7, co8, co9= st.beta_columns(9)
+with co5:
     buttom=st.button("O👌K")
 #buttom=st.button("👌")
 if buttom:
